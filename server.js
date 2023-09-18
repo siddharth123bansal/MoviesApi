bf1259e500de6b6e9f8046718c25fa1b69ff8bd9
siddharth <chinubansal766@gmail.com>
2023-09-19 01:06:06 +0530

    url change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 const PORT = 3001 
 const cors = require('cors');
 const corsOptions = {
-  origin: 'https://movieswebsite-seven.vercel.app',
+  origin: 'https://movieswebsite-nmhummhzb-siddharth123bansal.vercel.app',
 };
 app.use(cors(corsOptions));
 app.post('/movies/add', async (req, res) => {
